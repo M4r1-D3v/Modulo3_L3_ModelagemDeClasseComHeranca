968f9c084b26f9e0ee33c0a00ed5e4385d4e06ce
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-09-25 05:17:28 -0300

    Criação da subclasse Colaborador, métodos getters e setters e calcularSalario

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	 Fornecedor fornecedor = new Fornecedor("Ana","Rio de Janeiro","586903329", 700,400);
+	 Colaborador colaborador1 = new Colaborador("Maria","Recife", "563821189",34,4500, 1500);
+	 //fornecedor.valorSaldo();
+	 //System.out.println(fornecedor.valorSaldo());
 
-	 fornecedor.valorSaldo();
-	 System.out.println(fornecedor.valorSaldo());
+		colaborador1.calcularSalario();
+		System.out.println(colaborador1.calcularSalario());
     }
 }
